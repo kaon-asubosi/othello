7dbb71463c15e3c7551a1a604be666ec6616d93d
kaon-asubosi <gamepley2022@gmail.com>
2025-04-26 03:11:02 +0900

    リファクタリングが難しすぎた、次からは最速を出せるように練習をしたい

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,11 +18,9 @@ export default function Home() {
   const clickHandler = (x: number, y: number) => {
     console.log(x, y);
     const newBoard = structuredClone(board);
-    newBoard[y][x] = turnColor;
-    if (turnColor === 1) {
-      setTurnColor(2);
-    } else {
-      setTurnColor(1);
+    if (board[y + 1] !== undefined && board[y + 1][x] === 2 / turnColor) {
+      newBoard[y][x] = turnColor;
+      setTurnColor(2 / turnColor);
     }
     setBoard(newBoard);
   };
