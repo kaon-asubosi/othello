19d75e38177b38580c2bc6f14481c65f3535f4a5
taka <gamepley2022@gmail.com>
2025-04-29 17:32:53 +0900

    バグ修正が完了して殆ど完成と言って良い状態になった、最後に一つだけ相手のコマの上からコマを直接置けてしまうバグを確認したのでその修正をすればメイン部分は完成だと思われる

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,7 +31,6 @@ export default function Home() {
   const onearrow = (y: number, x: number, y_direction: number, x_direction: number) => {
     const candidate = [];
     let flag = 0;
-    let flag2 = 0;
     for (
       let i = y, p = x;
       board[i] !== undefined && board[i][p] !== undefined && (board[i][p] !== 0 || flag === 0);
@@ -40,30 +39,30 @@ export default function Home() {
       if (board[i][p] === turnColor) {
         if (2 <= flag) {
           console.log('check commit');
-          const newBoard = structuredClone(board);
-          for (const c of candidate) {
-            newBoard[c[0]][c[1]] = turnColor;
-          }
-          setBoard(newBoard);
-          flag2 += 1;
+          return candidate;
         }
         break;
       }
       candidate.push([i, p]);
       flag += 1;
     }
-    return flag2;
+    return [];
   };
 
   const eightarrow = (x: number, y: number) => {
-    let counter = 0;
+    let candidate = [];
     for (const direction of directions) {
       console.log(direction);
-      counter += onearrow(y, x, direction[0], direction[1]);
+      candidate = [...candidate, ...onearrow(y, x, direction[0], direction[1])];
     }
+    console.log(candidate);
     console.log('check1');
-    console.log(counter);
-    if (counter > 0) {
+    if (candidate.length > 0) {
+      const newBoard = structuredClone(board);
+      for (const c of candidate) {
+        newBoard[c[0]][c[1]] = turnColor;
+      }
+      setBoard(newBoard);
       console.log('check2');
       setTurnColor(2 / turnColor);
     }
